498278ef185a15a103c72f6555de3d7fcfd61724
Victoria Moore <victoriamoore@Victorias-MacBook-Pro.local>
2020-04-29 18:37:41 -0700

    npm i knex

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'production',
-    DATABASE_URL: process.env.DATABASE_URL || 'https://safe-chamber-60095.herokuapp.com/api',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'https://safe-chamber-60095.herokuapp.com/api',
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'https://safe-chamber-60095.herokuapp.com',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'https://safe-chamber-60095.herokuapp.com',
 };
